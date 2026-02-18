ad882a18e79c046d4012d0cb3d602b61b6d390be
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-02-18 14:53:00 +0700

    feat: setup betterAuth

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
-const app = express()
+const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-}
+};
 
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(cookieParser())
+app.use(cors(corsOptions));
+app.all("/api/auth/*", toNodeHandler(auth));
+app.use(express.json());
+app.use(cookieParser());
 
-export default app
+export default app;
