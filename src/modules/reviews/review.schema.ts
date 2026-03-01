c2b52f87629f8dd8adfd6333b2a50d1e4afd32e2
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-03-01 23:50:36 +0700

    refractor: normalize review to document per review instead of list in course document

--- src/modules/reviews/review.schema.ts
+++ src/modules/reviews/review.schema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import { z } from "zod";
 
 export const createReviewSchema = z.object({
   userId: z.string().min(1, "User ID is required"),
@@ -6,22 +6,23 @@ export const createReviewSchema = z.object({
   difficulty: z.number().min(1).max(5),
   content: z.string().min(1, "Content is required"),
   isAnonymous: z.boolean().default(false),
-})
+});
 
 export const updateReviewSchema = z.object({
   userId: z.string().min(1, "User ID is required"),
+  reviewId: z.string().min(1, "Review ID is required"),
   difficulty: z.number().min(1).max(5),
   content: z.string().min(1),
-})
+});
 
 export const voteReviewSchema = z.object({
   reviewId: z.string().min(1, "Review ID is required"),
-})
+});
 
 export const courseIdParamsSchema = z.object({
   courseId: z.string().min(1, "Course ID is required"),
-})
+});
 
-export type CreateReviewSchema = z.infer<typeof createReviewSchema>
-export type UpdateReviewSchema = z.infer<typeof updateReviewSchema>
-export type VoteReviewSchema = z.infer<typeof voteReviewSchema>
+export type CreateReviewSchema = z.infer<typeof createReviewSchema>;
+export type UpdateReviewSchema = z.infer<typeof updateReviewSchema>;
+export type VoteReviewSchema = z.infer<typeof voteReviewSchema>;
