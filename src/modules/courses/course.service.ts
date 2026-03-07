9ff21786f7caf7d912f89df033df4828cee0c064
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-03-08 00:52:03 +0700

    feat: NF3

--- src/modules/courses/course.service.ts
+++ src/modules/courses/course.service.ts
@@ -1,9 +1,9 @@
-import prisma from "@/shared/lib/prisma"
-import { AppError, ErrorCode } from "@/shared/lib/errors"
+import prisma from "@/shared/lib/prisma";
+import { AppError, ErrorCode } from "@/shared/lib/errors";
 
 export class CourseService {
   async getAllCourses() {
-    return prisma.course.findMany()
+    return prisma.course.findMany();
   }
 
   async getCourseById(id: string) {
@@ -11,22 +11,28 @@ export class CourseService {
       where: {
         id,
       },
-    })
+    });
     if (!course) {
-      throw new AppError(ErrorCode.COURSE_NOT_FOUND, "Course not found")
+      throw new AppError(ErrorCode.COURSE_NOT_FOUND, "Course not found");
     }
-    return course
+    return course;
   }
 
   async getAllCourseOfferings() {
     const courseOfferings = await prisma.course.findMany({
       include: {
-        offerings: true,
+        offerings: {
+          include: {
+            instructors: {
+              include: { instructor: true },
+            },
+          },
+        },
       },
-    })
+    });
 
-    return courseOfferings
+    return courseOfferings;
   }
 }
 
-export const courseService = new CourseService()
+export const courseService = new CourseService();
