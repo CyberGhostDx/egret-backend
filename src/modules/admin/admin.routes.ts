90cb6efac432fb31c63c63876c3b9207cafcb23d
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-03-07 15:17:35 +0700

    feat(admin): add review management

--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
 import { adminController } from "./admin.controller";
-import { requireAdmin } from "../../shared/middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/dashboard", requireAdmin, adminController.getDashboardInfo);
-router.get(
-  "/courses/offerings",
-  requireAdmin,
-  adminController.getAllCourseOfferingsWithExam,
-);
-router.post("/exam", requireAdmin, adminController.createExams);
-
+router.get("/dashboard", adminController.getDashboardInfo);
+router.get("/courses/offerings", adminController.getAllCourseOfferingsWithExam);
+router.get("/reviews", adminController.getAllReviews);
+router.delete("/reviews/:reviewId", adminController.softDeleteReviewByReviewId);
+router.post("/exam", adminController.createExams);
 export default router;
